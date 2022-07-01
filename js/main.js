948034e489fcd96394d5645c0a2d5750c89643bc
Chorong Shin <chorongstellashin@gmail.com>
2022-07-01 23:20:20 +0000

    Inspecting css design

--- js/main.js
+++ js/main.js
@@ -24,76 +24,21 @@ const listClick = document.querySelector('a.list-click');
 const $noResult = document.querySelector('p.no-result');
 const $form = document.querySelector('form.form');
 
-var count = 0;
-var imageList = [];
-var $fishId;
-var fishNames = [];
+let count = 0;
+let imageList = [];
+let $fishId;
+const fishNames = [];
 
-$fishResult.addEventListener('click', fishDetails);
-
-$previousIcon.addEventListener('click', previous);
-$nextIcon.addEventListener('click', next);
-
-function getFishDataList() {
-  var xhr = new XMLHttpRequest(name);
-  xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl);
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', function () {
-
-    data.fishList = xhr.response;
-    for (var fishId = 0; fishId < 100; fishId++) {
-
-      var $fish = document.createElement('li');
-      $fish.setAttribute('id', fishId);
-
-      var fishColumn = document.createElement('div');
-      fishColumn.setAttribute('class', 'fish-column');
-
-      var fishCard = document.createElement('div');
-      fishCard.setAttribute('class', 'fish-card');
-
-      var fishCardImage = document.createElement('div');
-      fishCardImage.setAttribute('class', 'fish-card-image');
-
-      var fishImage = document.createElement('img');
-      fishImage.setAttribute('src', xhr.response[fishId]['Species Illustration Photo'].src);
-      fishImage.setAttribute('alt', xhr.response[fishId]['Species Illustration Photo'].title);
-
-      var fishName = document.createElement('p');
-      fishName.setAttribute('class', 'fish-name');
-      fishName.textContent = xhr.response[fishId]['Species Name'];
-
-      var learnMoreCard = document.createElement('div');
-      learnMoreCard.setAttribute('class', 'learn-more-card');
-
-      var learnText = document.createElement('a');
-      learnText.setAttribute('class', 'learn-text');
-      learnText.setAttribute('href', '#');
-      learnText.textContent = 'LEARN MORE';
-
-      $fish.append(fishColumn);
-      fishColumn.append(fishCard);
-      fishCard.append(fishCardImage);
-      fishCardImage.append(fishImage);
-      fishCard.append(fishName);
-      fishCard.append(learnMoreCard);
-      learnMoreCard.append(learnText);
-      $fishList.append($fish);
-    }
-
-  });
-
-  xhr.send();
-}
-
-function fishDetails(event) {
+$fishResult.addEventListener('click', event => {
+  const closest = event.target.closest('ul > li');
+  $fishId = Number(closest.getAttribute('id'));
 
   // var $fish = document.querySelector('ul.fish-list > li');
   // $fishId = $fish.id;
   // console.log('fishId', $fishId);
 
   // NOTE: this assumes path[4] is always the LI. Can't change the HTML
-  $fishId = event.path[4].getAttribute('id');
+  // $fishId = event.path[4].getAttribute('id');
 
   // if we've never visited here, then liked is undefined, then
   // we should define with default values
@@ -119,14 +64,14 @@ function fishDetails(event) {
     $fishLocation.textContent = 'No Data Available';
     $fishLocation.setAttribute('class', 'no-li');
   } else {
-    var locationUL = data.fishList[$fishId].Location.trim();
-    var locationArray = locationUL.split('\n');
-    for (var i = 0; i < locationArray.length; i++) {
+    const locationUL = data.fishList[$fishId].Location.trim();
+    const locationArray = locationUL.split('\n');
+    for (let i = 0; i < locationArray.length; i++) {
       if (locationArray[i] === '<ul>' || locationArray[i] === '</ul>') {
         locationArray.splice(i, 1);
       }
     }
-    var locationString = locationArray.join(' ');
+    const locationString = locationArray.join(' ');
     $fishLocationList.innerHTML = locationString;
   }
 
@@ -134,18 +79,18 @@ function fishDetails(event) {
     $fishBiology.textContent = 'No Data Available';
     $fishBiology.setAttribute('class', 'no-li');
   } else if (data.fishList[$fishId].Biology !== null) {
-    var biologyUL = data.fishList[$fishId].Biology.trim();
-    var biologyArray = biologyUL.split('\n');
-    for (var j = 0; j < biologyArray.length; j++) {
+    const biologyUL = data.fishList[$fishId].Biology.trim();
+    const biologyArray = biologyUL.split('\n');
+    for (let j = 0; j < biologyArray.length; j++) {
       if (biologyArray[j] === ' <ul>' || biologyArray[j] === ' </ul>') {
         biologyArray.splice(j, 1);
       }
 
     }
 
-    var biologyString = biologyArray.join(' ');
+    const biologyString = biologyArray.join(' ');
     $fishBiologyList.innerHTML = biologyString;
-    var uls = document.querySelectorAll('ul.fish-biology-list > ul');
+    const uls = document.querySelectorAll('ul.fish-biology-list > ul');
     uls.forEach(ul => ul.classList.add('fish-biology-list'));
   }
 
@@ -195,7 +140,176 @@ function fishDetails(event) {
   handleView('fish');
   $fishIcon.removeEventListener('click', handleFishLikeClick);
   $fishIcon.addEventListener('click', handleFishLikeClick);
+});
+
+$previousIcon.addEventListener('click', previous);
+$nextIcon.addEventListener('click', next);
+
+function getFishDataList() {
+  const xhr = new XMLHttpRequest(name);
+  xhr.open('GET', 'https://lfz-cors.herokuapp.com/?url=' + targetUrl);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+
+    data.fishList = xhr.response;
+    for (let fishId = 0; fishId < 100; fishId++) {
+
+      const $fish = document.createElement('li');
+      $fish.setAttribute('id', fishId);
+      data.fishList[fishId].id = fishId;
+
+      const fishColumn = document.createElement('div');
+      fishColumn.setAttribute('class', 'fish-column');
+
+      const fishCard = document.createElement('div');
+      fishCard.setAttribute('class', 'fish-card');
+
+      const fishCardImage = document.createElement('div');
+      fishCardImage.setAttribute('class', 'fish-card-image');
+
+      const fishImage = document.createElement('img');
+      fishImage.setAttribute('src', xhr.response[fishId]['Species Illustration Photo'].src);
+      fishImage.setAttribute('alt', xhr.response[fishId]['Species Illustration Photo'].title);
+
+      const fishName = document.createElement('p');
+      fishName.setAttribute('class', 'fish-name');
+      fishName.textContent = xhr.response[fishId]['Species Name'];
+
+      const learnMoreCard = document.createElement('div');
+      learnMoreCard.setAttribute('class', 'learn-more-card');
+
+      const learnText = document.createElement('a');
+      learnText.setAttribute('class', 'learn-text');
+      learnText.setAttribute('href', '#');
+      learnText.textContent = 'LEARN MORE';
+
+      $fish.append(fishColumn);
+      fishColumn.append(fishCard);
+      fishCard.append(fishCardImage);
+      fishCardImage.append(fishImage);
+      fishCard.append(fishName);
+      fishCard.append(learnMoreCard);
+      learnMoreCard.append(learnText);
+      $fishList.append($fish);
+    }
+
+  });
+
+  xhr.send();
 }
+
+$fishList.addEventListener('click', event => {
+  const closest = event.target.closest('ul > li');
+  $fishId = Number(closest.getAttribute('id'));
+
+  // var $fish = document.querySelector('ul.fish-list > li');
+  // $fishId = $fish.id;
+  // console.log('fishId', $fishId);
+
+  // NOTE: this assumes path[4] is always the LI. Can't change the HTML
+  // $fishId = event.path[4].getAttribute('id');
+
+  // if we've never visited here, then liked is undefined, then
+  // we should define with default values
+  if (data.fishList[$fishId].liked === undefined) {
+    data.fishList[$fishId].liked = { fishId: $fishId, isLiked: false };
+  }
+
+  // make fish orange or blue based on whether it was liked
+  if (data.fishList[$fishId].liked.isLiked) {
+    $fishIcon.className = 'fish orange';
+    $fishIcon.src = 'images/fish-hook.png';
+  } else {
+    $fishIcon.className = 'fish blue';
+    $fishIcon.src = 'images/fish icon.png';
+  }
+
+  $fishIcon.setAttribute('id', $fishId);
+
+  $fishName.textContent = data.fishList[$fishId]['Species Name'];
+  $fishScientificName.textContent = data.fishList[$fishId]['Scientific Name'];
+
+  if (data.fishList[$fishId].Location === null) {
+    $fishLocation.textContent = 'No Data Available';
+    $fishLocation.setAttribute('class', 'no-li');
+  } else {
+    const locationUL = data.fishList[$fishId].Location.trim();
+    const locationArray = locationUL.split('\n');
+    for (let i = 0; i < locationArray.length; i++) {
+      if (locationArray[i] === '<ul>' || locationArray[i] === '</ul>') {
+        locationArray.splice(i, 1);
+      }
+    }
+    const locationString = locationArray.join(' ');
+    $fishLocationList.innerHTML = locationString;
+  }
+
+  if (data.fishList[$fishId].Biology === null) {
+    $fishBiology.textContent = 'No Data Available';
+    $fishBiology.setAttribute('class', 'no-li');
+  } else if (data.fishList[$fishId].Biology !== null) {
+    const biologyUL = data.fishList[$fishId].Biology.trim();
+    const biologyArray = biologyUL.split('\n');
+    for (let j = 0; j < biologyArray.length; j++) {
+      if (biologyArray[j] === ' <ul>' || biologyArray[j] === ' </ul>') {
+        biologyArray.splice(j, 1);
+      }
+
+    }
+
+    const biologyString = biologyArray.join(' ');
+    $fishBiologyList.innerHTML = biologyString;
+    const uls = document.querySelectorAll('ul.fish-biology-list > ul');
+    uls.forEach(ul => ul.classList.add('fish-biology-list'));
+  }
+
+  if (data.fishList[$fishId].Population === null) {
+    $fishPopulation.textContent = 'No Data Available';
+    $fishPopulation.setAttribute('class', 'population');
+  } else {
+    $fishPopulation.textContent = data.fishList[$fishId].Population;
+    $fishPopulation.setAttribute('class', 'population');
+  }
+
+  if (data.fishList[$fishId]['Image Gallery'] === null) {
+
+    imageList = [];
+    $fishImage.setAttribute('src', data.fishList[$fishId]['Species Illustration Photo'].src);
+    $fishImage.setAttribute('alt', data.fishList[$fishId]['Species Illustration Photo'].alt);
+
+    $previousIcon.classList.remove('view');
+    $nextIcon.classList.remove('view');
+    $previousIcon.classList.add('hidden');
+    $nextIcon.classList.add('hidden');
+
+  } else if (data.fishList[$fishId]['Image Gallery'].length === undefined) {
+    imageList = [];
+    $fishImage.setAttribute('src', data.fishList[$fishId]['Image Gallery'].src);
+    $fishImage.setAttribute('alt', data.fishList[$fishId]['Image Gallery'].alt);
+
+    $previousIcon.classList.remove('view');
+    $nextIcon.classList.remove('view');
+    $previousIcon.classList.add('hidden');
+    $nextIcon.classList.add('hidden');
+
+  } else if (data.fishList[$fishId]['Image Gallery'].length > 1) {
+
+    imageList = data.fishList[$fishId]['Image Gallery'];
+
+    $fishImage.setAttribute('src', imageList[0].src);
+    $fishImage.setAttribute('alt', imageList[0].alt);
+
+    $previousIcon.classList.remove('hidden');
+    $nextIcon.classList.remove('hidden');
+    $previousIcon.classList.add('view');
+    $nextIcon.classList.add('view');
+
+  }
+
+  handleView('fish');
+  $fishIcon.removeEventListener('click', handleFishLikeClick);
+  $fishIcon.addEventListener('click', handleFishLikeClick);
+});
 
 function previous() {
   if (count <= 0) {
@@ -214,8 +328,8 @@ function next() {
 
 }
 
-var handleFishLikeClick = function (event) {
-  var inputFishId = $fishIcon.getAttribute('id');
+const handleFishLikeClick = function (event) {
+  const inputFishId = Number($fishIcon.getAttribute('id'));
 
   if (inputFishId === data.fishList[inputFishId].liked.fishId && data.fishList[inputFishId].liked.isLiked === false) {
 
@@ -254,7 +368,6 @@ function setImg() {
   }
 }
 
-$fishList.addEventListener('click', fishDetails);
 $listClick.addEventListener('click', function () {
   handleView('list');
   $form.reset();
@@ -285,10 +398,10 @@ searchClick.addEventListener('click', function (event) {
   $resultList.innerHTML = '';
   handleView('search');
 
-  var fishData = data.fishList;
+  const fishData = data.fishList;
 
-  for (var i = 0; i < fishData.length; i++) {
-    var fishName = fishData[i]['Species Name'].toLowerCase();
+  for (let i = 0; i < fishData.length; i++) {
+    const fishName = fishData[i]['Species Name'].toLowerCase();
     fishNames.push(fishName);
 
   }
@@ -299,36 +412,36 @@ $form.addEventListener('submit', function (event) {
   event.preventDefault();
   $resultList.innerHTML = '';
 
-  var searchValue = $searchInput.value.toLowerCase();
+  const searchValue = $searchInput.value.toLowerCase();
 
-  for (var i = 0; i < 100; i++) {
+  for (let i = 0; i < 100; i++) {
 
     if (fishNames[i].indexOf(searchValue) > -1) {
 
-      var $fish = document.createElement('li');
+      const $fish = document.createElement('li');
       $fish.setAttribute('id', i);
 
-      var fishColumn = document.createElement('div');
+      const fishColumn = document.createElement('div');
       fishColumn.setAttribute('class', 'fish-column');
 
-      var fishCard = document.createElement('div');
+      const fishCard = document.createElement('div');
       fishCard.setAttribute('class', 'fish-card');
 
-      var fishCardImage = document.createElement('div');
+      const fishCardImage = document.createElement('div');
       fishCardImage.setAttribute('class', 'fish-card-image');
 
-      var fishImage = document.createElement('img');
+      const fishImage = document.createElement('img');
       fishImage.setAttribute('src', data.fishList[i]['Species Illustration Photo'].src);
       fishImage.setAttribute('alt', data.fishList[i]['Species Illustration Photo'].title);
 
-      var fishName = document.createElement('p');
+      const fishName = document.createElement('p');
       fishName.setAttribute('class', 'fish-name');
       fishName.textContent = data.fishList[i]['Species Name'];
 
-      var learnMoreCard = document.createElement('div');
+      const learnMoreCard = document.createElement('div');
       learnMoreCard.setAttribute('class', 'learn-more-card');
 
-      var learnText = document.createElement('a');
+      const learnText = document.createElement('a');
       learnText.setAttribute('class', 'learn-text');
       learnText.setAttribute('href', '#');
       learnText.textContent = 'LEARN MORE';
@@ -360,7 +473,7 @@ $form.addEventListener('submit', function (event) {
 
 function handleView(viewData) {
   data.view = viewData;
-  for (var i = 0; i < $views.length; i++) {
+  for (let i = 0; i < $views.length; i++) {
     if ($views[i].getAttribute('data-view') === viewData) {
       $views[i].className = 'view';
     } else {
