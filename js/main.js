cc36bb8de014546f576e6e90912db388c1f6615f
Chorong Shin <chorongstellashin@gmail.com>
2022-08-27 22:49:37 +0000

    Completed

--- js/main.js
+++ js/main.js
@@ -24,11 +24,6 @@ const searchClick = document.querySelector('a.search-click-two');
 const listClick = document.querySelector('a.list-click');
 const $noResult = document.querySelector('p.no-result');
 const $form = document.querySelector('form.form');
-const $menu = document.querySelector('.menu');
-const $menuItems = document.querySelectorAll('.menu-item');
-const $hamburger = document.querySelector('.hamburger');
-const $closeIcon = document.querySelector('.close-icon');
-const $menuIcon = document.querySelector('.menu-icon');
 
 let count = 0;
 let imageList = [];
@@ -458,21 +453,3 @@ function handleView(viewData) {
     }
   }
 }
-
-function toggleMenu() {
-  if ($menu.classList.contains('show-menu')) {
-    $menu.classList.remove('show-menu');
-    $closeIcon.style.display = 'none';
-    $menuIcon.style.display = 'block';
-  } else {
-    $menu.classList.add('show-menu');
-    $closeIcon.style.display = 'block';
-    $menuIcon.style.display = 'none';
-  }
-}
-
-$hamburger.addEventListener('click', toggleMenu);
-
-$menuItems.forEach(menuItem => {
-  menuItem.addEventListener('click', toggleMenu);
-});
